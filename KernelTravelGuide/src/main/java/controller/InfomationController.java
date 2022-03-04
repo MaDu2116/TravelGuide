ebe32da54e1f003d2eb7d9213b4c7a25cfdf91e2
MaDu2116 <100189480+MaDu2116@users.noreply.github.com>
2022-03-04 17:07:32 +0700

    ManhND: Update code InfomationController.java

--- KernelTravelGuide/src/main/java/controller/InfomationController.java
+++ KernelTravelGuide/src/main/java/controller/InfomationController.java
@@ -39,45 +39,47 @@ public class InfomationController extends HttpServlet {
 
 		String city = req.getParameter("city");
 		System.out.println(city);
+		int totalPage = 1;
+		
+		int currentPage = req.getParameter("page") != null ? Integer.valueOf(req.getParameter("page")) : 1;
 
 		switch (action) {
 		case "showAllTouristSpot":
-
-			int totalPage = infomationLogic.totalPage("showAllTouristSpot");
-
+			totalPage = infomationLogic.totalPage("showAllTouristSpot");
 			req.setAttribute("totalPage", totalPage);
-			int currentPage = req.getParameter("page") != null ? Integer.valueOf(req.getParameter("page")) : 1;
 			List<TouristSpotDto> touristSpotDtoList = new ArrayList<>();
-
 			touristSpotDtoList = infomationLogic.getAllTouristSpot(currentPage);
-
 			req.setAttribute("touristSpotDtoList", touristSpotDtoList);
-
 			dispatcher = req.getRequestDispatcher("/Views/Information/ShowAllTouristSpot.jsp");
 			break;
 
 		case "showAllHotel":
+			totalPage = infomationLogic.totalPage("showAllHotel");
+			req.setAttribute("totalPage", totalPage);
 			List<HotelDto> hotelDtoList = new ArrayList<>();
 			hotelDtoList = infomationLogic.getAllHotel();
 			req.setAttribute("hotelDtoList", hotelDtoList);
 			dispatcher = req.getRequestDispatcher("/Views/Information/ShowAllHotel.jsp");
 			break;
 		case "showAllRestaurant":
-			// ToDo
+			totalPage = infomationLogic.totalPage("showAllRestaurant");
+			req.setAttribute("totalPage", totalPage);
 			List<RestaurantDto> restaurantDtoList = new ArrayList<>();
 			restaurantDtoList = infomationLogic.getAllRestaurant();
 			req.setAttribute("restaurantDtoList", restaurantDtoList);
 			dispatcher = req.getRequestDispatcher("/Views/Information/ShowAllRestaurant.jsp");
 			break;
 		case "showAllResort":
-			// ToDo
+			totalPage = infomationLogic.totalPage("showAllResort");
+			req.setAttribute("totalPage", totalPage);
 			List<ResortDto> resortDtoList = new ArrayList<>();
 			resortDtoList = infomationLogic.getAllResort();
 			req.setAttribute("resortDtoList", resortDtoList);
 			dispatcher = req.getRequestDispatcher("/Views/Information/ShowAllResort.jsp");
 			break;
 		case "showAllTravel":
-			// ToDo
+			totalPage = infomationLogic.totalPage("showAllTravel");
+			req.setAttribute("totalPage", totalPage);
 			List<TravelDto> travelDtoList = new ArrayList<>();
 			travelDtoList = infomationLogic.getAllTravel();
 			req.setAttribute("travelDtoList", travelDtoList);
